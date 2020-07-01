be5ee89eee8d5a27c699f24f6dae705c5d06289f
barrozaum <igorbarrozo@hotmail.com>
2020-07-01 08:40:36 -0300

    commit dos exemplos feitos na sulamerica
    Curso vue

---
+++ curso-vue/componentes/componente-desafio/src/main/sub/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<v-app>
+		<v-navigation-drawer app>
+			<Menu/>
+		</v-navigation-drawer>
+		<Carrousel/>
+		<Toolbar/>
+		<Footer/>
+	</v-app>
+</template>
+
+<script>
+import Footer from '@/components/template/Footer.vue'
+import Carrousel from '@/components/widgets/Carrousel.vue'
+import Toolbar from '@/components/template/Toolbar.vue'
+import Menu from '@/components/template/Menu.vue'
+
+export default {
+	components : {  Footer, Carrousel,  Toolbar,  Menu},
+	
+}
+</script>
